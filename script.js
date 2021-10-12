680fb06dddb1a07482e6f15d079db06c12dc69d7
davidlizotte <47873116+davidlizotte@users.noreply.github.com>
2021-10-12 17:27:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,13 +37,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
   
   document.querySelector("#linkcreateaccount").addEventListener("click", e =>{
     e.preventDefault();
-    loginForm.classList.add("form-hidden");
-    createAccountForm.classList.remove("form-hidden");
+    loginform.classList.add("form-hidden");
+    createaccountform.classList.remove("form-hidden");
   });
   document.querySelector("#linklogin").addEventListener("click", e =>{
     e.preventDefault();
-    loginForm.classList.remove("form-hidden");
-    createAccountForm.classList.add("form-hidden");
+    loginform.classList.remove("form-hidden");
+    createaccountform.classList.add("form-hidden");
                                                           
 });
 
